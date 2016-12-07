28dc24e51f231c06445469c23c39af1f824d4df0
Alex Gubbay <gusgub@hotmail.co.uk>
2016-12-07 17:38:58 +0000

    Initial Commit

---
+++ Accelerometer Graph/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Accelerometer Graph
+//
+//  Created by Alex Gubbay on 07/12/2016.
+//  Copyright © 2016 Alex Gubbay. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
