baf8fcdb556339e32df1379e665e26ddfaa19e55
Alex Gubbay <gusgub@hotmail.co.uk>
2017-01-17 19:09:13 +0000

    Added functionality for timer picker

--- Accelerometer Graph/dataCaptureTableViewController.swift
+++ Accelerometer Graph/dataCaptureTableViewController.swift
@@ -1,5 +1,5 @@
 //
-//  TimePickerViewController.swift
+//  dataCaptureTableViewController.swift
 //  Accelerometer Graph
 //
 //  Created by Alex Gubbay on 17/01/2017.
@@ -8,31 +8,46 @@
 
 import UIKit
 
-class TimePickerViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+class dataCaptureTableViewController: UITableViewController, UIPickerViewDelegate, UIPickerViewDataSource {
 
-  
-    
     var minuteData = [["0","1","2","3","4","5"],["0","10","20","30","40","50"]]
     var minuteSelection = 0.0
     var secondSelection = 0.0
-    
-    
-    @IBAction func donePressed(_ sender: UIBarButtonItem) {
-    }
+    @IBOutlet weak var pickerView: UIPickerView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+
         self.pickerView.dataSource = self
         self.pickerView.delegate = self
-        // Do any additional setup after loading the view.
+
+        pickerView.selectRow(2, inComponent: 0, animated: true)
+        pickerView.selectRow(3, inComponent: 1, animated: true)
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
 
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 4
+    }
+
+    
     func numberOfComponents(in: UIPickerView) -> Int {
         return 2
     }
@@ -54,25 +69,17 @@ class TimePickerViewController: UIViewController, UIPickerViewDelegate, UIPicker
             let secString = minuteData[1][row]
             secondSelection = Double(secString)!
         }
+        print("woop")
     }
     
-    @IBAction func unwindHome(_ segue: UIStoryboardSegue) {
-        // this is intentionally blank
-    }
-    
+    /*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
-        print("doing")
-     let destination = segue.destination as! dataCaptureTableViewController
-        print(minuteSelection)
-        print(secondSelection)
-        destination.minuteSelection  = minuteSelection
-        destination.secondSelection = secondSelection
     }
-    
+    */
 
 }
