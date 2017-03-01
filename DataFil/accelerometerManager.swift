b91d55f77274554a8996683f8e9f0dddb13379b8
Alex Gubbay <Alex@alexs-macbook-pro-3.local>
2017-03-01 12:00:55 +0000

    Enabled sample speed adjustment

--- DataFil/accelerometerManager.swift
+++ DataFil/accelerometerManager.swift
@@ -12,56 +12,59 @@ import CoreMotion
 
 
 class accelerometerManager{
-
+    
     lazy var manager = CMMotionManager()
     lazy var queue = OperationQueue()
     var count = 0
-    var updateRate = 60.0
+    var sampleRate = 50.0
+    
+    init(){
+        NotificationCenter.default.addObserver(self, selector: #selector(self.newDatasourceSettings), name: Notification.Name("newDatasourceSettings"), object: nil)
+    }
     func initaliseAccelerometer(){
         
         if manager.isAccelerometerAvailable{
-            
             if manager.isAccelerometerActive == false{
-                
-                manager.accelerometerUpdateInterval = 1.0/updateRate
-                
+                manager.accelerometerUpdateInterval = 1.0/sampleRate
             }else{
                 print("accelerometer busy")
             }
         }
-        
         if manager.isGyroAvailable {
-            
             if manager.isGyroActive == false {
-                
-                //manager.gyroUpdateInterval = 0.001
                 manager.startAccelerometerUpdates(to: queue,
-                                                  withHandler: {data, error in
-                                                    
-                                                    guard data != nil else{
-                                                        return
-                                                    }
-                                                    
-                                                   
-                                                   
-                                                    DispatchQueue.main.async{
-                                                        self.count += 1
-                                                        let accel = accelPoint(dataX: (data?.acceleration.x)!, dataY:(data?.acceleration.y)!, dataZ:(data?.acceleration.z)!, count:self.count)
-                                                        
-                                                        NotificationCenter.default.post(name: Notification.Name("newRawData"), object: nil, userInfo:["data":accel])
-                                                        
-                                                    }
-                                                    
-                                                })
+                    withHandler: {data, error in
+                        guard data != nil else{
+                            return
+                        }
+                        DispatchQueue.main.async{
+                            self.count += 1
+                            let accel = accelPoint(dataX: (data?.acceleration.x)!, dataY:(data?.acceleration.y)!, dataZ:(data?.acceleration.z)!, count:self.count)
+                            
+                            NotificationCenter.default.post(name: Notification.Name("newRawData"), object: nil, userInfo:["data":accel])
+                        }
+                })
             } else {
                 print("Gyro is already active")
             }
-            
         } else {
             print("Gyro isn't available")
         }
     }
-
+    
+    @objc func newDatasourceSettings(notification: NSNotification) {
+        let data = notification.userInfo as! Dictionary<String,Double>
+        sampleRate = data["sampleRate"]!
+        print(sampleRate)
+        if manager.isAccelerometerAvailable{
+            if manager.isAccelerometerActive != false{
+                manager.accelerometerUpdateInterval = 1.0/sampleRate
+            }else{
+                print("accelerometer not active")
+            }
+        }
+    }
+    
 }
 
 
--- DataFil/graphSettingsTableViewController.swift
+++ DataFil/graphSettingsTableViewController.swift
@@ -15,7 +15,7 @@ class graphSettingsTableViewController: UITableViewController {
 
     @IBOutlet weak var pointCountSelector: UISegmentedControl!
     @IBOutlet weak var singleGraphSelection: UISwitch!
-    @IBOutlet weak var sampeRate: UISegmentedControl!
+    @IBOutlet weak var sampleRateSelection: UISegmentedControl!
     @IBOutlet weak var autoScale: UISwitch!
    
     
@@ -38,23 +38,43 @@ class graphSettingsTableViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
-        return 2
+        return 4
     }
 
     @IBAction func graphLayoutSwitched(_ sender: UISwitch) {
      
-        notify()
+        notifyGraphSettings()
         
     }
     @IBAction func graphPointsChanged(_ sender: UISegmentedControl) {
         
-        notify()
+        notifyGraphSettings()
         
     }
+    
+    @IBAction func sampleRateChanged(_ sender: UISegmentedControl) {
+        
+        notifyDatasourceSettings()
+    }
    
-    func notify(){
+    func notifyGraphSettings(){
         let number = (pointCountSelector.selectedSegmentIndex + 1) * 100
         NotificationCenter.default.post(name: Notification.Name("newGraphSettings"), object: nil, userInfo:["singleView":singleGraphSelection.isOn,"pointsCount":number])
+    }
+    
+    func notifyDatasourceSettings(){
+        
+        var sampleRate = 10.0
+        if sampleRateSelection.selectedSegmentIndex == 1{
+            sampleRate = 30.0
+        }else if sampleRateSelection.selectedSegmentIndex == 2{
+            sampleRate = 60.0
+        }else if  sampleRateSelection.selectedSegmentIndex == 3{
+            sampleRate = 100.0
+        }
+        print("psoting")
+        NotificationCenter.default.post(name: Notification.Name("newDatasourceSettings"), object: nil, userInfo:["sampleRate":sampleRate])
+        
     }
 
 }
